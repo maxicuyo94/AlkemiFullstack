43d06414513186f3c94627b9d4a461964a0b06b9
maxicuyo94 <maxibc94@gmail.com>
2021-10-21 15:01:29 -0300

    boilerplate init

---
+++ api/src/routes/index.js
@@ -0,0 +1,12 @@
+const { Router } = require("express");
+// Importar todos los routers;
+// Ejemplo: const authRouter = require('./auth.js');
+const pokemon = require("./pokemon.js");
+
+const router = Router();
+
+// Configurar los routers
+// Ejemplo: router.use('/auth', authRouter);
+router.use("/pokemons", pokemon);
+
+module.exports = router;
