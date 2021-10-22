fb0dcdbb593ab8c11d0baa66ef943b2513e80386
maxicuyo94 <maxibc94@gmail.com>
2021-10-22 11:22:28 -0300

    refactoring server and persistent databas

--- api/src/routes/operation.js
+++ api/src/routes/operation.js
@@ -1,6 +1,17 @@
 const server = require("express").Router();
 const { Operation } = require("../db.js");
-server.post("/add", async (req, res, next) => {
+const controller = {};
+
+controller.getAll = async (req, res, next) => {
+  try {
+    let operations = await Operation.findAll();
+    res.send(operations);
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error);
+  }
+};
+controller.add = async (req, res, next) => {
   let { concept, amount, date, type } = req.body;
   try {
     let newOperation = await Operation.create({ concept, amount, date, type });
@@ -9,43 +20,49 @@ server.post("/add", async (req, res, next) => {
     console.log(error);
     res.status(400).send(error);
   }
-});
-
-server.get("/", async (req, res, next) => {
+};
+controller.delete = async (req, res, next) => {
+  let { id } = req.body;
   try {
-    let operations = await Operation.findAll();
-    res.send(operations);
+    Operation.destroy({
+      where: {
+        id,
+      },
+    });
+
+    res.send(req);
   } catch (error) {
     console.log(error);
     res.status(400).send(error);
   }
-});
+};
 
-// server.put("/", async (req, res, next) => {
+// controller.updateasync (req, res, next) => {
+//   try {
+//     res.send(req)
+//   } catch (error) {
+//     console.log(error);
+//     res.status(400).send(error);
+//   }
+// };
+
+// controller.balance= async (req, res, next) => {
 //   try {
 //     res.send(req);
 //   } catch (error) {
 //     console.log(error);
 //     res.status(400).send(error);
 //   }
-// });
+// }
 
-// server.delete("/", async (req, res, next) => {
-//   try {
-//     res.send(req);
-//   } catch (error) {
-//     console.log(error);
-//     res.status(400).send(error);
-//   }
-// });
+server.get("/", controller.getAll);
 
-// server.get("/balance", async (req, res, next) => {
-//   try {
-//     res.send(req);
-//   } catch (error) {
-//     console.log(error);
-//     res.status(400).send(error);
-//   }
-// });
+server.post("/add", controller.add);
+
+server.delete("/delete", controller.delete);
+
+// server.put("/update", controller.update)
+
+// server.get("/balance", controller.balance)
 
 module.exports = server;
